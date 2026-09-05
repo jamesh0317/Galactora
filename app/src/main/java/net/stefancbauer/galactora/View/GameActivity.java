e151bf95301538b684dfdb18b07e358c05b92879
serviio <serviio@serviio>
2026-09-05 19:02:01 -0400

    Modernize to AndroidX, AGP 8.5.2, Gradle 8.7

--- app/src/main/java/net/stefancbauer/galactora/View/GameActivity.java
+++ app/src/main/java/net/stefancbauer/galactora/View/GameActivity.java
@@ -2,7 +2,7 @@ package net.stefancbauer.galactora.View;
 
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
--- app/src/main/java/net/stefancbauer/galactora/View/OptionsActivity.java
+++ app/src/main/java/net/stefancbauer/galactora/View/OptionsActivity.java
@@ -1,6 +1,6 @@
 package net.stefancbauer.galactora.View;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import net.stefancbauer.galactora.R;
